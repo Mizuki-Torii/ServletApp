de9c4254872ff35593e7ea9180c644b852d987e0
nguyenphihung <nguyen.phi.hung.sys21@morijyobi.ac.jp>
2022-07-06 10:21:07 +0900

    ( u_menu.java, u_menu.jsp ) uploaded

---
+++ servlet01/src/servlet/u_menu.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/u_menu")
+public class u_menu extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = null;
+		request.setCharacterEncoding("UTF-8");
+		String action = request.getParameter("action");
+
+		RequestDispatcher dispatcher =
+				request.getRequestDispatcher("WEB-INF/jsp/u_menu.jsp");
+		dispatcher.forward(request, response);
+	}
+
+
+}
