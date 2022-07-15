08bee771a81d70c84dc85c7a9cc42eb25d322061
Mizuki Torii <m.torii.sys21@morijyobi.ac.jp>
2022-07-15 10:47:28 +0900

    7/15 mizuki updata

---
+++ servlet01/src/DAO/updataDAO.java
@@ -0,0 +1,42 @@
+package DAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import scopedata.Account;
+public class updataDAO {
+    public int AccountUpdate( Account account ) {
+        int f = 0;
+        Connection con = null;
+        try {
+            Class.forName("org.h2.Driver");
+             con = DriverManager.getConnection(
+                        "jdbc:h2:tcp://localhost/~/test", "sa", "longh2132109");
+                String sql = "delete from account where id = ?";
+                PreparedStatement pstmt = con.prepareStatement(sql);
+                    pstmt.setString( 1, account.getId() );
+                    f = pstmt.executeUpdate();
+                    if( f == 0 ) return 0;
+                    sql = "insert into account values(?,?,?,?,?,?)";
+                    pstmt = con.prepareStatement(sql);
+                    pstmt.setString( 1, account.getId() );
+                    pstmt.setString( 2, account.getPass() );
+                    pstmt.setString( 3, account.getName() );
+                    pstmt.setString( 4, account.getMail() );
+                    pstmt.setInt( 5, account.getAge() );
+                    pstmt.setString( 6, account.getTel() );
+                    f = pstmt.executeUpdate();
+            } catch( Exception e ) {
+                System.out.println( e.getMessage() );
+            }finally{
+                try {
+                    if( con != null )
+                        con.close();
+                }catch ( SQLException e ) {
+                    e.printStackTrace();
+                }
+         }
+         return f;
+    }
+}
